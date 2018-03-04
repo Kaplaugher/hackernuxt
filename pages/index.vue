2f508377107b4da7a8a62036ed6eec9238f9f559
Kaplaugher <kaplaugher94@gmail.com>
2018-03-04 07:22:23 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="f1 code">
+    <h1>Hello</h1>
+    {{ids}}
+    <ul>
+      <li v-for="user in users" :key="user.id">{{user.login}}</li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  computed: mapState([
+    'users',
+    'ids'
+  ])
+}
+</script>
+
+<style>
+
+</style>
